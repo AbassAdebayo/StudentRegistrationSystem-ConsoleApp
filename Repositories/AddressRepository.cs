67f3b22cd9b186a4579a7fa3449b84c785af929b
abassadebayo <greatmoh007@gmail.com>
2024-12-14 11:17:42 +0100

    create address

--- Repositories/AddressRepository.cs
+++ Repositories/AddressRepository.cs
@@ -11,4 +11,27 @@ public class AddressRepository
     {
         _connection = connection;
     }
+    
+    public bool CreateAddress(Address address)
+    {
+        string query =
+            @"insert into Addresses (street, city, state, zipCode, addressLine, country, regNumber) 
+            values(@Street, @City, @State, @ZipCode, @AddressLine, @Country, @RegNumber)";
+
+        int result = 0;
+        using (MySqlCommand command = new MySqlCommand(query, _connection))
+        {
+            command.Parameters.AddWithValue("@Street", address.Street);
+            command.Parameters.AddWithValue("@City", address.City);
+            command.Parameters.AddWithValue("@State", address.State);
+            command.Parameters.AddWithValue("@ZipCode", address.ZipCode);
+            command.Parameters.AddWithValue("@AddressLine", address.AddressLine);
+            command.Parameters.AddWithValue("@Country", address.Country);
+            command.Parameters.AddWithValue("@RegNumber", address.RegNumber);
+            
+            command.ExecuteNonQuery();
+        }
+        
+        return result == 1 ? true : false;
+    }
 }
